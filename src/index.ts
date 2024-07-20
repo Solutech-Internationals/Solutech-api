19560b6285a84c2092832605fd52a2037457e883
Ujitha <ujithamigara@gmail.com>
2024-07-20 12:45:39 +0530

    add laptop api endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import laptopRoutes from "./routes/laptopRoutes"
+import connectDB from "./config/db"
+
+
+const app = express();
+const port = process.env.APP_PORT || 8080;
+
+connectDB();
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/', laptopRoutes);
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port} `);
+});
